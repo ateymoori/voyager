cb28dbcd30d94c54e3c415defa01a66c9c6a56b7
AmirHossein Teymoori <teymoori.net@gmail.com>
2020-04-15 09:34:26 +0200

    story ctrl updated

--- app/Http/Controllers/Voyager/StoryController.php
+++ app/Http/Controllers/Voyager/StoryController.php
@@ -15,6 +15,15 @@ class StoryController extends Controller
         $all = Story::get() ;
         foreach ($all as $story) {
             $story['author']=Author::findOrFail($story['author']);
+            unset($story['updated_at']);
+            unset($story['created_at']);
+            unset($story['content']);
+            unset($story['mp3_file']);
+            unset($story['author']['updated_at']);
+            unset($story['author']['created_at']);
+            unset($story['author']['description']);
+            $story['image'] = url('/')."/storage/".$story['image'];
+            $story['author']['image'] = url('/')."/storage/".$story['author']['image'];
         }
         return response()->json($all) ;
      }
@@ -22,6 +31,12 @@ class StoryController extends Controller
      public function getByID($id){
          $story = Story::findOrFail($id) ;
          $story['author']=Author::findOrFail($story['author']);
+         $story['image'] = url('/')."/storage/".$story['image'];
+         $story['author']['image'] = url('/')."/storage/".$story['author']['image'];
+         if(  strlen($story['mp3_file'])>5){
+            $mp3= json_decode( $story['mp3_file'],true)  ;
+            $story['mp3_file'] =  url('/')."/storage/".$mp3[0]['download_link'];
+         }
         return response()->json($story) ;
      }
 }
